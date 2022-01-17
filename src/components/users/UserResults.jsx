c5cce7dc3f2828f165ea851f43e1ae4d89763385
mysterymethod <chandra.pranay@yahoo.in>
2022-01-17 15:27:28 +0530

    card component created for each user

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Spinner from '../layout/spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -21,16 +22,17 @@ function UserResults() {
     setLoading(false)
   }
 
-  if(!loading) {
+  if (!loading) {
     return (
-      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-        {users.map((user) => <h3>{user.login}</h3>)}
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
       </div>
     )
   } else {
     return <Spinner />
   }
-  
 }
 
 export default UserResults
