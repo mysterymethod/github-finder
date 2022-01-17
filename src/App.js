197e57052bed43bb35b8672523f3beef25c49f60
mysterymethod <chandra.pranay@yahoo.in>
2022-01-17 16:05:23 +0530

    moved state to context

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./components/layout/Footer";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import About from './pages/About'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
 
+import './App.css'
 
-import "./App.css";
-
-import Navbar from "./components/layout/Navbar";
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
+  )
 }
 
-export default App;
+export default App
