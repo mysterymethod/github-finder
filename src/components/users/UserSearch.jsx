025cb991015e264a2c8269bd3525dc9d609dce1e
mysterymethod <chandra.pranay@yahoo.in>
2022-01-18 12:00:52 +0530

    added functionality on search component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,14 +1,36 @@
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
 function UserSearch() {
+  const [text, setText] = useState('')
+
+  const { users } = useContext(GithubContext)
+
+  const handleChange = (event) => setText(event.target.value)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (text === '') {
+      alert('Please enter a valid Username')
+    } else {
+      // @todo - Search User
+      setText('')
+    }
+  }
+
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
                 type="text"
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="search"
+                value={text}
+                onChange={handleChange}
               />
               <button
                 type="submit"
@@ -20,9 +42,11 @@ function UserSearch() {
           </div>
         </form>
       </div>
-      <div>
-        <button className="btn btn-ghost btn-lg">CLEAR</button>
-      </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">CLEAR</button>
+        </div>
+      )}
     </div>
   )
 }
